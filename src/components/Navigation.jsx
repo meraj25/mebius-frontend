9d275505fb98ab2977e5be6fbf1cddc3d3c002c6
meraj25 <tambdevapriya@students.nsbm.ac.lk>
2025-08-24 01:10:58 +0530

    change nav height

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -28,7 +28,7 @@ export default function Navigation() {
     <header className="bg-white border-b border-gray-200 px-4 lg:px-16">
   
           
-         <div className=" flex items-center justify-between h-20">
+         <div className=" flex items-center justify-between h-16">
           <Link to="/" className="font-bold text-2xl m-8 ">
             Mebius
           </Link>
