40ce4714a2b25fdc0aad2cb792f52a48715b112b
meraj25 <tambdevapriya@students.nsbm.ac.lk>
2025-08-24 13:58:14 +0530

    .

--- src/pages/shopping.page.jsx
+++ src/pages/shopping.page.jsx
@@ -74,7 +74,7 @@ if (isLoading) {
         </PopoverContent>
       </Popover>
 
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
           <SimpleProductCard key={product._id} product={product} />
         ))}
