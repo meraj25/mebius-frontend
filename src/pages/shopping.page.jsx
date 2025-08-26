78f922b449d8e07ea9984cac17639ab80520d1d6
meraj25 <tambdevapriya@students.nsbm.ac.lk>
2025-08-26 20:19:59 +0530

    .

--- src/pages/shopping.page.jsx
+++ src/pages/shopping.page.jsx
@@ -54,7 +54,8 @@ const [ selectedColor, setSelectedColor ] = useState("");
 
 
 
-     <div>{filteredProducts.map((product) => (
+     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {filteredProducts.map((product) => (
         <SimpleProductCard key={product._id} product={product} />
       ))}
       </div>
