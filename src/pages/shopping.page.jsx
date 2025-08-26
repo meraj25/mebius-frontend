752bd7581a793e9475a9813f5be9b7d89fd44bb3
meraj25 <tambdevapriya@students.nsbm.ac.lk>
2025-08-26 21:11:35 +0530

    aligned two filters

--- src/pages/shopping.page.jsx
+++ src/pages/shopping.page.jsx
@@ -42,35 +42,46 @@ const [ selectedColor, setSelectedColor ] = useState("");
  
   return (
     <div>
-      <h2>Filter By: </h2>
-   
-      <Select
-        value={selectedColor}
-        onValueChange={(value) => setSelectedColor(value)}
-        >
-        <SelectTrigger>
-          <SelectValue placeholder="Select a color" />
-        </SelectTrigger>
-        <SelectContent>
-          {colors.map((color) => (
-            <SelectItem key={color._id} value={color._id}>
-              {color.name}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-      
- <h2 className="mt-4">Sort By Price: </h2>
-      <Select value={sortOrder} 
-      onValueChange={(value) => setSortOrder(value)}>
-        <SelectTrigger>
-          <SelectValue placeholder="Select order" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="asc">Price: Low to High</SelectItem>
-          <SelectItem value="desc">Price: High to Low</SelectItem>
-        </SelectContent>
-      </Select>
+      <div className="flex items-end gap-6 mb-6">
+        <div>
+          <h2>Filter By:</h2>
+          <Select
+            value={selectedColor}
+            onValueChange={(value) => setSelectedColor(value)}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Select a color" />
+            </SelectTrigger>
+            <SelectContent>
+              {colors.map((color) => (
+                <SelectItem key={color._id} value={color._id}>
+                  {color.name}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <div>
+          <h2>Sort By Price:</h2>
+          <Select
+            value={sortOrder}
+            onValueChange={(value) => setSortOrder(value)}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Select order" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="asc">Price: Low to High</SelectItem>
+              <SelectItem value="desc">Price: High to Low</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {filteredProducts.map((product) => (
+          <SimpleProductCard key={product._id} product={product} />
+        ))}
+      </div>
 
 
      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
